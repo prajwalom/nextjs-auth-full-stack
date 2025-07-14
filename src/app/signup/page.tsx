9670e7bc7cfc90e5cb0fd795b7da9068fc26b180
prajwalom <prajwalpadole99@gmail.com>
2025-07-14 16:23:22 +0530

    feat: enhance signup page layout and styling

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -16,15 +16,27 @@ export default function signUpPage(){
 
 
     return (
-        <div>
-            <h1>
+    <div>
+            <div>
+                <h1>
                 Please SignUp.. 
             </h1>
+            </div>
+        
+        <div className="flex flex-col justify-center items-center mt-20 ">
             <hr />
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" value={user.username} onChange={(e) => setUser({...user, username: e.target.value})}
+            <input className="pt-3" id="username" type="text" value={user.username} onChange={(e) => setUser({...user, username: e.target.value})}
             placeholder="username" />
 
+            <label className="pt-10" htmlFor="email">Email</label>
+            <input className="pt-3 rounded-full" id="email"
+            type="email"
+            value={user.email}
+            onChange={(e) => setUser ({...user, email: e.target.value})}
+            placeholder="Please Enter Your Email" />
         </div>
+
+    </div>
     )
 }
