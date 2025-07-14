0f4272cafa3a36393083c7ed6053b88647ff23ea
prajwalom <prajwalpadole99@gmail.com>
2025-07-14 11:04:38 +0530

    feat: add login page component

---
+++ src/app/login/loginpage.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return (
+        <div className="bg-black">
+        <h1 className="text-center text-red-600 text-7xl"> Please Login to Enter</h1>
+        </div>
+    )
+}
